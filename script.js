8eea74d77cd5ad49a7a4e14686464701fcd69922
David <david-bitner@hotmail.com>
2022-05-13 16:38:18 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+const overlay = document.querySelector(`.overlay`);
+const modalCard = document.querySelector(`#modal-card`);
+const modalNew = document.querySelector(`#modal-new`);
+const modalNewBtn = document.querySelector(`#btn-add-card`);
+const modalNewField = document.querySelector(`#btn-add-input`);
+
+class Card {}
+
+class Cards {}
+
+class UI {}
+
+//Listeners
+//Update UI
+//Add Card
+//Remove Card
+//Edit Card
+
+/* CARD
+<div class="card">
+  <div class="card__front card__side">
+    <div class="card__title">Card front title</div>
+  </div>
+  <div class="card__back card__side">
+    <div class="card__title">Card back title</div>
+    <div class="card__field">Card back field</div>
+    <div class="card__field">Card back field</div>
+    <div class="card__field">Card back field</div>
+    <div class="card__field">Card back field</div>
+  </div>
+</div>
+*/
+
